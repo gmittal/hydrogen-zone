e98d216ec53c8ce880cfa68e589e975b3b493436
Gautam Mittal <gautam@mittal.net>
2014-04-01 11:22:56 -0700

    Atom Generation

--- Projectfiles/HelloWorldLayer.m
+++ Projectfiles/HelloWorldLayer.m
@@ -55,13 +55,13 @@
         timeLabel.color = ccc3(12, 24, 43);
         [self addChild:timeLabel z:10001];
         
-        timeLeft = 131;
+        timeLeft = 90;
         
         
         
         
         // initialize various arrays
-        [self initAtoms:10]; // specify how many atoms
+        [self initAtoms:15]; // specify how many atoms to start with, and they'll automatically regenerate themselves!
         
         
         [self startTimer];
@@ -73,9 +73,27 @@
 }
 
 
+#pragma mark Functions That Generate Some Value
 -(int) generateRandNumberFrom:(int) fromNumber to:(int)toNumber {
     return (arc4random()%(toNumber-fromNumber+1))+fromNumber;
 }
+
+-(CGPoint) generatePointByAngle:(float) angle distance:(float) someDistance startPoint:(CGPoint) point
+{
+    //    NSLog(@"%f", angle);
+    angle = CC_DEGREES_TO_RADIANS(angle);
+    double addedX = sin(angle) * someDistance;
+    double addedY = cos(angle) * someDistance;
+    //    NSLog(@"ADDED X: %f", addedX);
+    //    NSLog(@"ADDED Y: %f", addedY);
+    //    NSLog(NSStringFromCGPoint(point));
+    CGPoint endPoint = ccp(point.x + addedX, point.y + addedY);
+    //     NSLog(NSStringFromCGPoint(endPoint));
+    //    NSLog(NSStringFromCGPoint(endPoint));
+    return endPoint;
+}
+
+
 
 
 -(void) initAtoms:(int) numAtoms {
@@ -86,9 +104,9 @@
     for (int i = 0; i < numAtoms; i++) {
         double type = ((double)arc4random() / ARC4RANDOM_MAX);
         int atomType = 0;
-        if (type < 0.75f) {
+        if (type < 0.85f) {
             atomType = 0; // OXYGEN! YAY!
-        } else if (type > 0.75f) {
+        } else if (type > 0.85f) {
             atomType = 1; // HALOGEN! OH NO!
         }
         
@@ -116,17 +134,40 @@
         
         tmp.scale = scale;
         
-        tmp.position = screenCenter;
+        int generatedPointAngle = [self generateRandNumberFrom:0 to:360];
+        
+        int xRand = [self generateRandNumberFrom:0 to:320];
+        int yRand = [self generateRandNumberFrom:0 to:480];
+        
+        CGPoint generationAxis = ccp(xRand, yRand);
+        
+        CGPoint spriteStart = [self generatePointByAngle:generatedPointAngle distance:500 startPoint:generationAxis];
+//        NSLog([NSString stringWithFormat:@"%@", spriteStart]);
+        tmp.position = spriteStart;
         
         
         [atoms addObject:tmp];
         [self addChild:tmp];
         
+        CGPoint endPoint = [self generatePointByAngle:generatedPointAngle+180 distance:500 startPoint:generationAxis];
+        
+//        [self moveObject:tmp toP1:[tmp position] P2:screenCenter];
+        float moveTime = 13.0f;
+        [tmp runAction:[CCMoveTo actionWithDuration:moveTime position:endPoint]];
+        [self performSelector:@selector(removeSprite:) withObject:tmp afterDelay:moveTime];
+        
     }
 }
 
+-(void) removeSprite:(CCSprite*) tmpSprite
+{
+    [self removeChild:tmpSprite cleanup:YES];
+    [atoms removeObject:tmpSprite];
+    [self initAtoms:1];
+}
 
 
+#pragma mark Timer Functions
 -(void) startTimer {
     [self schedule: @selector(tick:) interval:1.0];
     
@@ -162,8 +203,7 @@
 }
 
 
-
-
+#pragma mark UPDATE FUNCTIONS
 
 -(void) movePlayerPos: (CGPoint) rot_pos1 rot_pos2:(CGPoint) rot_pos2
 {
@@ -210,6 +250,53 @@
     
 }
 
+
+-(void) moveObject:(CCSprite*)object toP1:(CGPoint) rot_pos1 P2:(CGPoint) rot_pos2
+{
+    while (!CGPointEqualToPoint(object.position, rot_pos2)) {
+        float touchangle;
+        float rotation_theta = atan((rot_pos1.y-rot_pos2.y)/(rot_pos1.x-rot_pos2.x)) * 180 / M_PI;
+        
+        if(rot_pos1.y - rot_pos2.y > 0)
+        {
+            if(rot_pos1.x - rot_pos2.x < 0)
+            {
+                touchangle = (-90-rotation_theta);
+            }
+            else if(rot_pos1.x - rot_pos2.x > 0)
+            {
+                touchangle = (90-rotation_theta);
+            }
+        }
+        else if(rot_pos1.y - rot_pos2.y < 0)
+        {
+            if(rot_pos1.x - rot_pos2.x < 0)
+            {
+                touchangle = (270-rotation_theta);
+            }
+            else if(rot_pos1.x - rot_pos2.x > 0)
+            {
+                touchangle = (90-rotation_theta);
+            }
+        }
+        
+        if (touchangle < 0)
+        {
+            touchangle+=360;
+        }
+        
+        float speed = 1; // Move 50 pixels in 60 frames (1 second)
+        
+        float vx = cos(touchangle * M_PI / 180) * speed;
+        float vy = sin(touchangle * M_PI / 180) * speed;
+        
+        CGPoint direction = ccp(vy,vx);
+        
+        object.position = ccpAdd(player.position, direction);
+        object.rotation = touchangle;
+    }
+
+}
 
 
 
